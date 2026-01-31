9101fe5843d4992682c49eaf468f4fa4d30419c1
Fanganga <shanks.andy@gmail.com>
2026-01-31 11:21:18 +0000

    corrected http url

--- RedactionAPI/Program.cs
+++ RedactionAPI/Program.cs
@@ -3,15 +3,15 @@ using RedactionAPI.Utilities.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-int port = 8080;
-if(args.Length > 0)
-{
-    int.TryParse(args[0], out port );
-}
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.Listen(System.Net.IPAddress.Parse("127.0.0.1"), port);
-});
+//int port = 8080;
+//if(args.Length > 0)
+//{
+//    int.TryParse(args[0], out port );
+//}
+//builder.WebHost.ConfigureKestrel(serverOptions =>
+//{
+//    serverOptions.Listen(System.Net.IPAddress.Parse("127.0.0.1"), port);
+//});
 // Add services to the container.
 builder.Services.AddScoped<ICustomLogger, FileLogger>();
 builder.Services.AddSingleton<IInformationService, InformationService>();
